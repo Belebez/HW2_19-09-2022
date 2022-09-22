c60ffa47b23fd729b0db1e4f25e75fbc512412e2
Viktor <viktor.belebezyev@yandex.ru>
2022-09-22 11:12:21 +0300

    add 3 solution algorithm

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,23 +1,22 @@
 ﻿//  Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 //  первое решение
 
-Console.Write("Введите число - ");
-string number = Console.ReadLine();
-int len = number.Length;
-int num = int.Parse(number);
+// Console.Write("Введите число - ");
+// string number = Console.ReadLine();
+// int len = number.Length;
+// int num = int.Parse(number);
 
-int div = Convert.ToInt32(Math.Pow(10, (len - 3)));
+// int div = Convert.ToInt32(Math.Pow(10, (len - 3)));
 
-if (num > 99)  //  проверка есть ли третье число
-{
-    int num3 = (num / div) % 10;  //  получаем третье число 
-    Console.WriteLine($"Третья цифра в числе - {number}, является - {num3}");
-    
-}
-else
-{
-    Console.WriteLine($"В числе {number} нет третьего числа");
-}
+// if (num > 99)  //  проверка есть ли третье число
+// {
+//     int num3 = (num / div) % 10;  //  получаем третье число 
+//     Console.WriteLine($"Третья цифра в числе - {number}, является - {num3}");  
+// }
+// else
+// {
+//     Console.WriteLine($"В числе {number} нет третьего числа");
+// }
 
 
 // в python как-то проще это сделать)))
@@ -50,3 +49,22 @@ else
 //   int num3 = number % 10;
 //   Console.WriteLine($" является - {num3}");
 // }
+
+// третье решение
+
+Console.Write("Введите число - ");
+int number = int.Parse(Console.ReadLine());
+
+int num1 = Convert.ToInt32(Math.Log10(number));
+
+int div = Convert.ToInt32(Math.Pow(10, (num1 - 2)));
+
+if (number > 99)  //  проверка есть ли третье число
+{
+    int num3 = (number / div) % 10;  //  получаем третье число 
+    Console.WriteLine($"Третья цифра в числе - {number}, является - {num3}");  
+}
+else
+{
+    Console.WriteLine($"В числе {number} нет третьего числа");
+}
